d8b10e08f756b5bfaffb8ee473067bcb273bbbdb
MONISHM1 <monishshah795@gmail.com>
2026-07-12 22:27:48 +0530

    schema changed

--- schema.js
+++ schema.js
@@ -25,6 +25,14 @@ module.exports.listingSchema = Joi.object({
 });
 
 
+module.exports.reviewSchema = Joi.object({
+    review: Joi.object({
+        rating: Joi.number().min(1).max(5).required(),
+        comment: Joi.string().required(),
+    }).required(),
+});
+
+
 
 module.exports.bookingSchema = Joi.object({
 
